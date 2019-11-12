22fb06fdb9eab669f621a770681bf115a8d46755
Taissia George <taissia.george@khk.ee>
2019-11-12 15:45:38 +0200

    querySelector Stiili muutmine

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 // docemnt meetodid
 // single element selectors
 
-sisu = document.getElementById('add-task'); // HTML sisu
-sisu = document.getElementById('add-task').id; //id
-sisu = document.getElementById('add-task').className;
+sisu = document.querySelector('#title'); // id järgi
+sisu = document.querySelector('.card-title'); // klassi järgi
+sisu = document.querySelector('h5'); // elemendi järgi 
 
+// stiili muutmine
 
-sisu = document.getElementById('title');
-id = document.getElementById('title').id;
-klass = document.getElementById('title').className;
-
-// stiliseerimine
-
-sisu.style.background = '#333';
-document.getElementById('title').style.background = '#eee';
-sisu.style.color = '#fff';
-sisu.style.padding = '5px';
-// sisu.style.display = 'none';
-
-// sisu muutmine
-
-sisu.textContent = 'Minu Ülesanded';
-sisu.innerText = 'Just minu ülesanded';
-
-sisu.innerHTML = '<span style="color:green">Tegemata</span>'
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(2)').style.color = 'orange';
 
 console.log(sisu);
