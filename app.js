e5e453d68b00fa59f5585775709b64e401fd47a2
Taissia George <taissia.george@khk.ee>
2019-11-12 14:33:59 +0200

    OOP 1

---
+++ app.js
@@ -0,0 +1,46 @@
+  
+// window objects / methods / properties
+
+// alert
+// window.alert('Tere Hommikust!');
+// alert('Tere Hommikust!');
+
+// propt
+// const sisend = prompt('Sisesta oma nimi');
+// alert('Tere Hommikust, ' + sisend + '!');
+
+// confirm
+// if(confirm('Kas oled nõus?')){
+//   console.log('jah');
+// } else {
+//   console.log('ei');
+// }
+
+// välimõõdud
+// let valiKorgus = window.outerHeight;
+// let valiLaius = window.outerWidth;
+// console.log(valiLaius)
+
+// sisemõõdud
+// let siseKorgus = window.innerHeight;
+// let siseLaius = window.innerWidth;
+// console.log(siseLaius)
+
+// scroll
+// scrolY = window.scrollY;
+// scrolX = window.scrollX;
+// console.log(scrolY);
+
+// location objekt
+let objekt = window.location;
+let hostname = window.location.hostname;
+let port = window.location.port;
+let href = window.location.href;
+let search = window.location.search;
+
+// redirect
+// window.location.href = 'https://khk.ee';
+// reload
+// window.location.reload();
+
+console.log(search);
